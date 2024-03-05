d9ff8fab1664a528bdcb75d874077aeca5be0065
DikshaExternLabs <dikshadishu0608@gmail.com>
2024-03-05 22:48:07 +0100

    installed some packages and added chat engine in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine, ChatFeed } from 'react-chat-engine'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <ChatEngine
+          height='100vh'
+          projectID='d4fba653-4b15-4d34-98c0-29ece39df35c'
+          userName='Rob'
+          userSecret='qwert12345'
+          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        
+        />
+      </h1>
     </div>
   );
 }

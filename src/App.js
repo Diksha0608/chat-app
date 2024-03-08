eb40f69c177afd44dcf6d16e86841cf738d6d798
DikshaExternLabs <dikshadishu0608@gmail.com>
2024-03-08 17:03:57 +0100

    added login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { ChatEngine } from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        <ChatEngine
-          height='100vh'
-          projectID='d4fba653-4b15-4d34-98c0-29ece39df35c'
-          userName='Rob'
-          userSecret='qwert12345'
-          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-          onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+const projectID = 'd4fba653-4b15-4d34-98c0-29ece39df35c';
 
-        />
-      </h1>
-    </div>
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
 
 export default App;
